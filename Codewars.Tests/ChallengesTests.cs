e15fd42d702046b7d4003f46a5b3d752304f0cf5
Timo Baader <timo_baader@hotmail.de>
2024-07-28 14:48:53 +0300

    Simple Pig Latin

---
+++ Codewars.Tests/ChallengesTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using static Codewars.Challenges;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class PigIt
+    {
+        [TestCase("Pig latin is cool", "igPay atinlay siay oolcay")]
+        [TestCase("Hello world !", "elloHay orldway !")]
+        public void PigIt_ReturnsCorrectResult(string input, string expected)
+        {
+            string result = PigIt(input);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
---
+++ Codewars/Challenges.cs
@@ -0,0 +1,30 @@
+﻿namespace Codewars
+{
+    public class Challenges
+    {
+        // This project contains my solutions for codewars.com coding challenges. I will include the challenge text above every solution.
+        // I will also include the highest rated solution from codewars.com as a comment below my solution.
+
+        // Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+        public static string PigIt(string input)
+        {
+            var result = String.Empty;
+            var inputArray = input.Split(' ');
+            foreach (var str in inputArray)
+            {
+                if (str.All(char.IsLetter))
+                {
+                    result += $"{str.Substring(1, str.Length - 1)}{str[0]}ay ";
+                }
+                else
+                {
+                    result += $"{str} ";
+                }
+            }
+            result = result.TrimEnd(' ');
+            return result;
+
+            //  return string.Join(" ", str.Split(' ').Select(w => w.Any(char.IsPunctuation) ? w : w.Substring(1) + w[0] + "ay"));
+        }
+    }
+}
